a0f8f2024cdbdf68ee65eb471ec964aacba32b83
stanleyos <stanislaw.sarka@gmail.com>
2023-08-26 09:25:02 +0200

    Fix API_URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 export const API_URL =
-  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:8000"
+  process.env.NODE_ENV === "production" ? "/" : "http://localhost:8000"
 
 export const IMGS_URL =
   process.env.NODE_ENV === "production"
